5c0c9c78759418eaa411594f6668143c0f6beb99
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2024-07-31 11:09:12 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/crud_flutter_v1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.crud_flutter_v1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,109 @@
+import 'package:crud_flutter_v1/models/task.dart';
+import 'package:crud_flutter_v1/services/database_service.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final DatabaseService _databaseService = DatabaseService.instance;
+
+  String? _task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: _addTaskButton(),
+      body: _tasksList(),
+    );
+  }
+
+  Widget _addTaskButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('Add Task'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _task = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Subscribe...',
+                  ),
+                  autofocus: true,
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  onPressed: () {
+                    if (_task == null || _task == '') return;
+                    _databaseService.addTask(_task!);
+                    setState(() {
+                      _task = null;
+                    });
+                    Navigator.pop(
+                      context,
+                    );
+                  },
+                  child: const Text(
+                    'Done',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      child: const Icon(
+        Icons.add,
+      ),
+    );
+  }
+
+  Widget _tasksList() {
+    return FutureBuilder(
+      future: _databaseService.getTask(),
+      builder: (context, snapshot) {
+        return ListView.builder(
+          itemCount: snapshot.data?.length ?? 0,
+          itemBuilder: (context, index) {
+            Task task = snapshot.data![index];
+            return ListTile(
+              onLongPress: () {
+                _databaseService.deleteTask(
+                  task.id,
+                );
+                setState(() {});
+              },
+              title: Text(task.content),
+              trailing: Checkbox(
+                value: task.status == 1,
+                onChanged: (value) {
+                  _databaseService.updateTaskStatus(
+                    task.id,
+                    value == true ? 1 : 0,
+                  );
+                  setState(() {});
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
